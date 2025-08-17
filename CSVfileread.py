2a0796262e83d43f1bdc38379f25f8a4d25303b7
gokul@techorc.com <gokul@techorc.com>
2025-08-17 15:17:56 +0530

    Initial commit

---
+++ CSVfileread.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+a = [1,2,3]
+
+df = pd.DataFrame(a)
+
+print(df)
---
+++ sqlite.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+conn = sqlite3.connect('C:/Users/user/Desktop/4pychram/DB/test.db')
+
+print('opened database sucessfully')
+
+#conn.execute('''CREATE TABLE orders
+ #               (OrderID int not null primary key,
+  #              Price REAL not null,
+   #             Product text not null,
+    #            CustomerID INT not null,
+     #           CustomerName text not null);''')
+
+#print('table created sucessful')
+
+#conn.execute("insert into orders(OrderID,Price,Product,CustomerID,CustomerName) values (1,10.2,'apple',101,'sam');")
+#conn.execute("insert into orders(OrderID,Price,Product,CustomerID,CustomerName) values (2,20.2,'spple',201,'ram');")
+#conn.execute("insert into orders(OrderID,Price,Product,CustomerID,CustomerName) values (3,30.2,'gpple',301,'jam');")
+#conn.commit()
+#print('records added sucesssfully')
+
+a = conn.execute("select * from orders;")
+for i in a:
+    print(i)
+
+#conn.execute("update orders set CustomerName = 'pam' where CustomerID = 301;")
+#conn.commit()
+#print(str(conn.total_changes) + ': total changes no')
